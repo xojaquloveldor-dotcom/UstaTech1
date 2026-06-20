d7a3d5b01e20374099029af17e7824919d4ad758
dawdawdaw <awdawdawdaw@gmail.com>
2026-06-20 15:01:20 +0500

    Kola

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const AuthPage = () => {
   const [isLogin, setIsLogin] = useState(true);
